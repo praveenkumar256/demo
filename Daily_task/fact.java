85d5f6d09e1bd479335d320522bffdc3926f4260
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-06 09:36:58 +0530

    daily task

---
+++ Daily_task/fact.java
@@ -0,0 +1,37 @@
+package dsa_java;
+
+import java.util.Scanner;
+
+public class fact {
+    
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int fact = factorial(n);
+        System.out.println("Factorial of " + n + " is: " + fact);
+
+        System.out.print("Fibonacci series up to " + n + " terms: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+
+        sc.close();
+    }
+}
